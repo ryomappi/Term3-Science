df1281a24b18dbb9a14ddb6d17e5c7a628b10bcf
Ryoma Ishida <ryomappi0910@gmail.com>
2024-01-23 00:30:38 +0900

    denoise機能まで実装した

--- RamanSpectrum_repository.py
+++ RamanSpectrum_repository.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
-from pathlib import Path
+from scipy.sparse import csc_matrix
+from scipy.sparse import spdiags
+import scipy.sparse.linalg as spla
+import numpy as np
+from scipy import signal
+import csv
 
 
 class RamanSpectrum:
     def __init__(self, img_path):
-        self.img_path = f'data/img/{img_path}'
+        self.img_path = f"data/img/{img_path}"
+        self.paramAsLS = [10**3.5, 0.00005]  # paramAsLS = [ lam , p ]
+        self.paramSG = [80, 5]  # paramSG = [ dn , poly ]
 
     def read_img(self):
         return cv2.imread(self.img_path, cv2.IMREAD_GRAYSCALE)
 
     def pixel_to_wavenumber(self, pixel):
         # kaiserの式のマジックナンバーたちは、設計に基づいた数値であることに注意
-        kaiser = 1.87970*10**4 - 10**7/(532 + (633 - 532)/1440 * pixel)
+        kaiser = 1.87970 * 10**4 - 10**7 / (532 + (633 - 532) / 1440 * pixel)
         return kaiser
 
     def get_raman_intensity(self):
@@ -25,7 +32,7 @@ class RamanSpectrum:
             for y in range(height):
                 intensity_of_pixel = img[y, x]
                 sum_intensity += intensity_of_pixel
-            raman_intensity.append(sum_intensity/height)
+            raman_intensity.append(sum_intensity / height)
         return raman_intensity
 
     def get_raman_wavenumber(self):
@@ -39,14 +46,84 @@ class RamanSpectrum:
     def get_spectrum(self, file_name):
         raman_intensity = self.get_raman_intensity()
         raman_wavenumber = self.get_raman_wavenumber()
-        plt.xlabel('Wavenumber (cm-1)')
-        plt.ylabel('Raman Intensity')
+        plt.xlabel("Wavenumber (cm-1)")
+        plt.ylabel("Raman Intensity")
         plt.plot(raman_wavenumber, raman_intensity)
-        plt.savefig(f'data/img/{file_name}.png')
+        plt.savefig(f"data/img/{file_name}.png")
 
     def save_spectrum(self, file_name):
-        df = pd.DataFrame({
-            'Wavenumber': self.get_raman_wavenumber(),
-            'Intensity': self.get_raman_intensity()
-        })
-        df.to_csv(f'data/csv/{file_name}.csv')
+        df = pd.DataFrame(
+            {
+                "Wavenumber": self.get_raman_wavenumber(),
+                "Intensity": self.get_raman_intensity(),
+            }
+        )
+        df.to_csv(f"data/csv/{file_name}.csv")
+
+    def open_csv(self, file_name):
+        X = []
+        Y = []
+        with open(file_name, "r") as f:
+            header = next(csv.reader(f))
+            reader = csv.reader(f)
+            data = [row for row in reader]
+            for i in range(len(data)):
+                X.append(float(data[i][1]))
+                Y.append(float(data[i][2]))
+        return X, Y
+
+    def baseline_als(self, y, lam, p, niter=10):  # AsLSによりベースライン推定
+        # https://stackoverflow.com/questions/29156532/python-baseline-correction-library
+        # p: 0.001 - 0.1, lam: 10^2 - 10^9
+        # Baseline correction with asymmetric least squares smoothing, P. Eilers, 2005
+        L = len(y)
+        D = csc_matrix(np.diff(np.eye(L), 2))
+        w = np.ones(L)
+        for i in range(niter):
+            W = spdiags(w, 0, L, L)
+            Z = W + lam * D.dot(D.transpose())
+            z = spla.spsolve(Z, w * y)
+            w = p * (y > z) + (1 - p) * (y < z)
+        return z
+
+    def SGs(self, y, dn, poly):  # Savitzky-Golyによりノイズ除去
+        # y as np.array, dn as int, poly as int
+        n = len(y) // dn
+        if n % 2 == 0:
+            N = n + 1
+        elif n % 2 == 1:
+            N = n
+        else:
+            print("window length can't set as odd")
+        SGsmoothed = signal.savgol_filter(y, window_length=N, polyorder=poly)
+        return SGsmoothed
+
+    def outFigCSV(self, file_name):  # baseline estimation and smoothing
+        X, Y = self.open_csv(file_name)
+        paramAsLS = self.paramAsLS
+        paramSG = self.paramSG
+        Y_np = np.array(Y)
+        # baseline estimation
+        bkg = self.baseline_als(Y_np, paramAsLS[0], paramAsLS[1])
+        fix = Y_np - bkg  # remove baseline
+        # smoothing
+        smth = self.SGs(fix, paramSG[0], paramSG[1])
+
+        # csv output
+        dataOutput = np.c_[X, Y, bkg, smth]
+        np.savetxt(f"data/csv/{file_name}.csv", dataOutput, delimiter=",")
+
+        # figures
+        plt.figure(figsize=(12, 9))
+        ax1 = plt.subplot2grid((2, 2), (0, 0), colspan=2)
+        ax2 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
+
+        ax1.plot(X, Y, linewidth=2)
+        ax1.plot(X, bkg, "b", linewidth=1, linestyle="dashed", label="baseline")
+
+        ax2.plot(X, fix, "g", linewidth=1, linestyle="dashed", label="remove baseline")
+        ax2.plot(X, smth, "b", linewidth=2, label="smoothed")
+
+        plt.legend()
+        plt.axis("tight")
+        plt.show()
--- raman.py
+++ raman.py
@@ -4,15 +4,16 @@ from MyException import *
 from pathlib import Path
 
 
-def run(img_path, output_path) -> None:
+def run(img_path, spectrum_output_path, denoised_output_path) -> None:
     raman_spectrum = RamanSpectrum(img_path)
-    raman_spectrum.get_spectrum(output_path)
-    raman_spectrum.save_spectrum(output_path)
+    raman_spectrum.get_spectrum(spectrum_output_path)
+    raman_spectrum.save_spectrum(spectrum_output_path)
+    raman_spectrum.outFigCSV(denoised_output_path)
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) != 3:
-        raise MissingArgumentError('Missing Argument: <image path name> <output path name>')
+        raise MissingArgumentError('Missing Argument: <image_path> <spectrum_output_path> <denoised_output_path>')
     if not Path(f'data/img/{args[1]}').exists():
         raise MissingFileError(f'Missing File: {args[1]}')
-    run(args[1], args[2])
+    run(args[1], args[2], args[3])
